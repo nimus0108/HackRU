4383bc4231a3f1f85595772ba90ee775e0521392
LiamRahav <liorogamerdvd@gmail.com>
2014-10-11 18:02:35 -0400

    Made the replace math/symbols function in js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 //Essay Shortener!!!!!!!
 
-var sample = "Sample essay."
+var sample = "Sample essay.";
+
+function replacemath(essay){
+    var random = Math.floor((Math.random() * 2) + 1)
+    if random == 1{ 
+        essay = essay.replace("+", "added with")
+    }
+    if random == 2{
+        essay = essay.replace("+", "plus")
+    }
+    essay = essay.replace("%"," percent")
+    essay = essay.replace(" ÷"," divided by")
+    essay = essay.replace("=","equals")
+    essay = essay.replace("&","and")
+    essay = essay.replace("minus","subtracted by")
+    essay = essay.replace(" -"," subtracted by")
+    essay = essay.replace(" x "," multiplied by ")
+    essay = essay.replace(" *"," multiplied by")
+    essay = essay.replace(" /"," divided by")
+    essay = essay.replace("^","raised to the power of")
+    return essay
+}
 

9115a40dae1e07a0c117ef63853af6530a3ff060
SarahNathanson <ilovemypugisabelle@gmail.com>
2014-10-12 02:13:02 -0400

    too tired to message

--- js/input.js
+++ js/input.js
@@ -3,9 +3,9 @@ function input(){
         var essay= document.getElementById("textinput").innerHTML;
         document.getElementsByClassName("long").onclick=function(){
             return finalreplace(essay);
-        }
+        };
         document.getElementsByClassName("short").onclick=function(){
             return finalshorten(essay);
-        }
+        };
     }
 }

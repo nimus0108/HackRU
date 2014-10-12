dcc1860f28bad8ca0c144f6c3df28dc866bedb49
SarahNathanson <sarahnathanson@nbp-170-45.nbp.ruw.rutgers.edu>
2014-10-12 01:00:57 -0400

    added .innerHTML

--- js/input.js
+++ js/input.js
@@ -1,6 +1,6 @@
 function input(){
     document.getElementById("submitbutton").onclick=function(){
-        var essay= document.getElementById("textinput");
+        var essay= document.getElementById("textinput").innerHTML;
         document.getElementsByClassName("long").onclick=function(){
             return finalreplace(essay);
         }
